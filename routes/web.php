3acf3a11db9f7d6746f5af8d69269fafa6e12753
Paulo <pescanellasotero@gmail.com>
2026-03-06 11:24:39 +0100

    Creación de los rutas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,30 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LlibreController;
+use App\Http\Controllers\AutorController;
+use App\Http\Controllers\LlibreAutorController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Libros
+Route::get('/llibres', [LlibreController::class, 'index']);
+Route::get('/llibres/{id}', [LlibreController::class, 'show']);
+Route::post('/llibres', [LlibreController::class, 'store']);
+Route::put('/llibres/{id}', [LlibreController::class, 'update']);
+Route::delete('/llibres/{id}', [LlibreController::class, 'destroy']);
+
+//Autores
+Route::get('/autors', [AutorController::class, 'index']);
+Route::get('/autors/{id}', [AutorController::class, 'show']);
+Route::post('/autors', [AutorController::class, 'store']);
+Route::put('/autors/{id}', [AutorController::class, 'update']);
+Route::delete('/autors/{id}', [AutorController::class, 'destroy']);
+
+//Relación N-N
+Route::post('/llibres/{id}/autors/{autor_id}', [LlibreAutorController::class, 'assignarAutor']);
+Route::delete('/llibres/{id}/autors/{autor_id}', [LlibreAutorController::class, 'eliminarAutor']);
+Route::get('/llibres/{id}/autors', [LlibreAutorController::class, 'autorsDeLlibre']);
+Route::get('/autors/{id}/llibres', [LlibreAutorController::class, 'llibresDeAutor']);
